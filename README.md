389bcb1deff72ae34998900c0fbf677d0fa05e74
MrodAnd <165328870+MrodAnd@users.noreply.github.com>
2024-05-01 21:05:22 -0400

    additions

---
+++ README.md
@@ -0,0 +1,3 @@
+# BikeandTruckroutes
+
+This file shows bike lane and truck routes in NYC. It also shows what strees have both bike lanes and truck routes.
---
+++ js/scripts.js
@@ -0,0 +1,162 @@
+
+// go to map
+mapboxgl.accessToken = 'pk.eyJ1IjoibXJvZGEiLCJhIjoiY2x1dHl3cXM5MDRmZTJscDZuczk1MGR0MyJ9.b4OLZlubx4b5Hc19pGZRpQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v11',
+    center: [-73.99579, 40.69942],
+    zoom: 10.5
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+// execute load
+map.on('load', () => {
+
+
+    // layers
+    console.log(
+        map.getStyle().layers
+    )
+
+
+    // trucks GIS Data
+    map.addSource('trucks', {
+        "type": "geojson",
+        "data": "data/trucks.geojson"
+    })
+
+    // trucks line layer data
+    map.addLayer({
+        'id': 'trucks',
+        'type': 'line',
+        'source': 'trucks',
+        'layout': {},
+        'paint': {
+            'line-color': '#FFA500',
+            'line-width': 2.5
+        }
+    },);
+
+    // trucks fill layer data
+    map.addLayer({
+        'id': 'trucks',
+        'type': 'fill',
+        'source': 'trucks',
+        'layout': {},
+        'paint': {
+        }
+    },);
+
+
+    // bikes GIS data
+    map.addSource('bikes', {
+        "type": "geojson",
+        "data": "data/bikes.geojson"
+    })
+
+    // Line layer Data bikes
+    map.addLayer({
+        'id': 'bikes',
+        'type': 'line',
+        'source': 'bikes',
+        'layout': {},
+        'paint': {
+            'line-color': '#028A0F',
+            'line-width': 2.5
+        }
+    },);
+
+    // fill layer bikes
+    map.addLayer({
+        'id': 'bikes',
+        'type': 'fill',
+        'source': 'bikes',
+        'layout': {},
+        'paint': {
+        }
+    },);
+
+    // bike and trucks shapefile
+    map.addSource('bikeandtrucks', {
+        "type": "geojson",
+        "data": "data/bikeandtrucks.geojson"
+    })
+
+    // bike and trucks line
+    map.addLayer({
+        'id': 'bikeandtrucks',
+        'type': 'line',
+        'source': 'bikeandtrucks',
+        'layout': {},
+        'paint': {
+            'line-color': '#ce2d2d',
+            'line-width': 2.5
+        }
+    },);
+
+    // bike and trucks fill layer
+    map.addLayer({
+        'id': 'bikeandtrucks',
+        'type': 'fill',
+        'source': 'bikeandtrucks',
+        'layout': {},
+        'paint': {
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+
+            ],
+            'fill-opacity': 0.7
+        }
+    },);
+    // bike, truck, and bike+truck route button visibility info
+    let biketruckvisible = true
+
+    $('#bikeandtrucks-toggle').on('click', function () {
+
+        let value = 'visible'
+
+        if (biketruckvisible === true) {
+            value = 'none'
+        }
+
+        map.setLayoutProperty('bikeandtrucks', 'visibility', value)
+        map.setLayoutProperty('bikeandtrucks', 'visibility', value)
+
+        biketruckvisible = !biketruckvisible
+    })
+
+    let bikevisible = true
+
+    $('#bike-toggle').on('click', function () {
+
+        let value = 'visible'
+
+        if (bikevisible === true) {
+            value = 'none'
+        }
+
+        map.setLayoutProperty('bikes', 'visibility', value)
+        map.setLayoutProperty('bikes', 'visibility', value)
+
+        bikevisible = !bikevisible
+    })
+
+    let trucksvisible = true
+
+    $('#trucks-toggle').on('click', function () {
+
+        let value = 'visible'
+
+        if (trucksvisible === true) {
+            value = 'none'
+        }
+
+        map.setLayoutProperty('trucks', 'visibility', value)
+        map.setLayoutProperty('trucks', 'visibility', value)
+
+        trucksvisible = !trucksvisible
+    })
+
+})
